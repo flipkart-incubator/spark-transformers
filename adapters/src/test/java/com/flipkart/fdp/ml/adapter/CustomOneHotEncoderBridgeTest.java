6a39bfbf8d14777da22ea2a0693b115466d13831
Shubhranshu Shekhar <shubhranshu.cse@gmail.com>
2016-07-28 01:03:16 +0530

    Incorporating review comments for CustomOneHot

--- adapters/src/test/java/com/flipkart/fdp/ml/adapter/CustomOneHotEncoderBridgeTest.java
+++ adapters/src/test/java/com/flipkart/fdp/ml/adapter/CustomOneHotEncoderBridgeTest.java
@@ -6,6 +6,7 @@ import com.flipkart.fdp.ml.export.ModelExporter;
 import com.flipkart.fdp.ml.importer.ModelImporter;
 import com.flipkart.fdp.ml.transformer.Transformer;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.ml.feature.OneHotEncoder;
 import org.apache.spark.ml.feature.StringIndexer;
 import org.apache.spark.ml.feature.StringIndexerModel;
 import org.apache.spark.mllib.linalg.Vector;
@@ -23,6 +24,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by shubhranshu.shekhar on 21/06/16.
@@ -60,20 +62,34 @@ public class CustomOneHotEncoderBridgeTest  extends SparkTestBase {
         //Export this model
         byte[] exportedModel = ModelExporter.export(sparkModel, indexed);
 
+        //Create spark's OneHotEncoder
+        OneHotEncoder sparkOneHotModel = new OneHotEncoder()
+                .setInputCol("categoryIndex")
+                .setOutputCol("categoryVec");
+
         //Import and get Transformer
         Transformer transformer = ModelImporter.importAndGetTransformer(exportedModel);
 
         //compare predictions
         Row[] sparkOutput = sparkModel.transform(indexed).orderBy("id").select("id", "categoryIndex", "categoryVec").collect();
-        for (Row row : sparkOutput) {
+        Row[] sparkOneHotOutput = sparkOneHotModel.transform(indexed).orderBy("id").select("id", "categoryIndex", "categoryVec").collect();
 
+        //Compare Spark's OneHotEncoder with CustomOneHotEncoder
+        //See if the dictionary size is equal
+        assertEquals(sparkOutput.length, sparkOneHotOutput.length);
+
+        for (int i = 0; i < sparkOutput.length; i++) {
+            Row row= sparkOutput[i];
             Map<String, Object> data = new HashMap<String, Object>();
             data.put(sparkModel.getInputCol(), row.getDouble(1));
             transformer.transform(data);
             double[] transformedOp = (double[]) data.get(sparkModel.getOutputCol());
 
             double[] sparkOp = ((Vector) row.get(2)).toArray();
+            //get spark's OneHotEncoder output
+            double[] sparkOneHotOp = ((Vector) sparkOneHotOutput[i].get(2)).toArray();
             assertArrayEquals(transformedOp, sparkOp, 0.01);
+            assertArrayEquals(sparkOneHotOp, sparkOp, 0.01);
         }
     }
 }
