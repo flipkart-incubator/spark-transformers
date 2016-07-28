0b181f20af6e24e0d55156288293fa551d5bac5e
Akshay Utkarsh Sharma <akshay.us@flipkart.com>
2016-07-28 09:13:09 +0530

    1.refactor for java 7 2. minor fixes

--- models-info/src/main/java/com/flipkart/fdp/ml/transformer/VectorAssemblerTransformer.java
+++ models-info/src/main/java/com/flipkart/fdp/ml/transformer/VectorAssemblerTransformer.java
@@ -2,8 +2,8 @@ package com.flipkart.fdp.ml.transformer;
 
 import com.flipkart.fdp.ml.modelinfo.VectorAssemblerModelInfo;
 
+import java.util.ArrayList;
 import java.util.Map;
-import java.util.stream.Stream;
 
 /**
  *
@@ -21,30 +21,30 @@ public class VectorAssemblerTransformer implements Transformer {
 
     private double[] predict (Object[] inputs) {
 
-        int vectorSize = Stream.of(inputs)
-                .filter(o->isTypeDouble(o) || isTypeDoubleArray(o))
-                .mapToInt(o -> isTypeDoubleArray(o) ? ((double[])o).length : 1)
-                .sum();
-
-        double[] output = new double[vectorSize];
+        ArrayList<Double> output = new ArrayList<>();
         int i=0;
         for (Object input : inputs) {
             if (input == null) {
                 throw new RuntimeException("Values to assemble cannot be null");
             }
             else if (isTypeDouble(input)) {
-                output[i++] = (double) input;
+                output.add((double)input);
             }
             else if (isTypeDoubleArray(input)) {
                 for (double val : (double[])input) {
-                    output[i++] = val;
+                    output.add(val);
                 }
             }
             else {
                 throw new RuntimeException("Values to assemble cannot be of type: " + input.getClass().getCanonicalName());
             }
         }
-        return output;
+        double[] primitiveOutput = new double[output.size()];
+        i=0;
+        for(Double val : output) {
+            primitiveOutput[i++] = val;
+        }
+        return primitiveOutput;
     }
 
     private boolean isTypeDouble(Object o) {
