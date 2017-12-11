c9b935caf44fb252694ad0575af7e6587a60cb85
gaurav.prasad <gaurav.prasad@flipkart.com>
2017-12-11 14:13:45 +0530

    Reverting formatting

--- adapters-2.0/src/test/java/com/flipkart/fdp/ml/adapter/LogisticRegressionBridgeTest.java
+++ adapters-2.0/src/test/java/com/flipkart/fdp/ml/adapter/LogisticRegressionBridgeTest.java
@@ -19,33 +19,33 @@ import static org.junit.Assert.assertEquals;
 
 public class LogisticRegressionBridgeTest extends SparkTestBase {
 
-	@Test
-	public void testLogisticRegression() {
-		//prepare data
-		String datapath = "src/test/resources/binary_classification_test.libsvm";
-		JavaRDD<LabeledPoint> trainingData = MLUtils.loadLibSVMFile(jsc.sc(), datapath).toJavaRDD();
+    @Test
+    public void testLogisticRegression() {
+        //prepare data
+        String datapath = "src/test/resources/binary_classification_test.libsvm";
+        JavaRDD<LabeledPoint> trainingData = MLUtils.loadLibSVMFile(jsc.sc(), datapath).toJavaRDD();
 
-		//Train model in spark
-		LogisticRegressionModel lrmodel = new LogisticRegressionWithSGD().run(trainingData.rdd());
+        //Train model in spark
+        LogisticRegressionModel lrmodel = new LogisticRegressionWithSGD().run(trainingData.rdd());
 
-		//Export this model
-		byte[] exportedModel = ModelExporter.export(lrmodel);
+        //Export this model
+        byte[] exportedModel = ModelExporter.export(lrmodel);
 
-		//Import and get Transformer
-		Transformer transformer = ModelImporter.importAndGetTransformer(exportedModel);
+        //Import and get Transformer
+        Transformer transformer = ModelImporter.importAndGetTransformer(exportedModel);
 
-		//validate predictions
-		List<LabeledPoint> testPoints = trainingData.collect();
-		for (LabeledPoint i : testPoints) {
-			Vector v = i.features();
-			double actual = lrmodel.predict(v);
+        //validate predictions
+        List<LabeledPoint> testPoints = trainingData.collect();
+        for (LabeledPoint i : testPoints) {
+            Vector v = i.features();
+            double actual = lrmodel.predict(v);
 
-			Map<String, Object> data = new HashMap<String, Object>();
-			data.put("features", v.toArray());
-			transformer.transform(data);
-			double predicted = (double) data.get("prediction");
+            Map<String, Object> data = new HashMap<String, Object>();
+            data.put("features", v.toArray());
+            transformer.transform(data);
+            double predicted = (double) data.get("prediction");
 
-			assertEquals(actual, predicted, 0.01);
-		}
-	}
+            assertEquals(actual, predicted, 0.01);
+        }
+    }
 }
